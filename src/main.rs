52c2f9e358b774040f5482b184a7e4699f5cbb24
zkoevaari <72908856+zkoevaari@users.noreply.github.com>
2024-11-11 13:23:48 +0100

    Testing commit with hello world.

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+/*
+    Created by Zoltan Kovari, 2024.
+
+    Licensed under the Apache License, Version 2.0
+    http://www.apache.org/licenses/LICENSE-2.0
+    (see LICENSE file)
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
